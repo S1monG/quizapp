cf33eeffe089c5c544ead804d229cc24ebf937cd
S1monG <si5564gu-s@student.lu.se>
2025-10-08 12:02:02 +0200

    WIP display quiz

--- src/components/DisplayQuiz.tsx
+++ src/components/DisplayQuiz.tsx
@@ -1,4 +1,20 @@
 import type { QuizData } from "@/types/QuizTypes"
+import {
+  Carousel,
+  CarouselContent,
+  CarouselItem,
+  CarouselNext,
+  CarouselPrevious,
+} from "@/components/ui/carousel"
+import {
+  Card,
+  CardAction,
+  CardContent,
+  CardDescription,
+  CardFooter,
+  CardHeader,
+  CardTitle,
+} from "@/components/ui/card"
 
 type PropType = {
     quiz: QuizData
@@ -6,7 +22,18 @@ type PropType = {
 function DisplayQuiz( {quiz}: PropType ) {
     return (
         <>
+            {quiz.results.map( (question, index) => (
+
+                    <div className="p-1">
+                        <Card>
+                            <CardContent className="flex items-center justify-center p-6">
+                                <span className="text-3xl font-semibold">{index + 1}</span>
+                                </CardContent>
+                            </Card>
+                        </div>
+                ))}
         </>
+
     )
 }
 
