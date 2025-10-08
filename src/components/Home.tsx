15d5a5cbf237fd8f267fd60d296d18c452de2a5c
Emelie Tingberg <emelietingberg@MacBook-Air-som-tillhor-Emelie.local>
2025-10-08 10:24:23 +0200

    lite på home

--- src/components/Home.tsx
+++ src/components/Home.tsx
@@ -2,7 +2,14 @@
 function Home() {
     return (
         <div>
-            <h1 className="text-4xl font-bold text-center">Home</h1>
+            <h1 className="text-4xl font-bold text-center mt-15">Welcome <br /> to <br />
+                <span className="text-6xl font-bold text-blue-200 drop-shadow-[0_0_1px_black]" >GuessQuest</span></h1>
+            <h3 className="text-xl text-center mt-10">Here at GuessQuest you can find several quiz categories with
+                 different levels of difficulty, as well as viewing your results! </h3>
+            <div className="grid grid-cols-2 gap-4 p-8">
+                <div className="bg-blue-200 p-4 text-xl font-bold text-center">Categories</div>
+                <div className="bg-blue-200 p-4 text-xl font-bold text-center">Results</div>
+            </div>
         </div>
     )
 }
--- src/components/ResultView.tsx
+++ src/components/ResultView.tsx
@@ -3,6 +3,8 @@ function ResultView() {
     return (
         <h1 className="text-4xl font-bold text-center">Result</h1>
     )
+
+    //nåt om använda outlet context för o nå quiz resultaten
 }
 
 export default ResultView
